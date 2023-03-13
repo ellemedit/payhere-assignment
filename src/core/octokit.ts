d45099fe9a9061e9f95c533b7692ab52deffc4e4
BaHwan <beingbook@gmail.com>
2023-03-13 13:23:26 +0900

    setup github api

--- src/core/octokit.ts
+++ src/core/octokit.ts
@@ -2,4 +2,27 @@ import { Octokit } from "octokit";
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
-export const octokit = new Octokit({ auth: GITHUB_TOKEN });
+const octokit = new Octokit({ auth: GITHUB_TOKEN });
+
+export function searchRepos(query: string) {
+  return octokit.rest.search.repos({ q: query, per_page: 40 });
+}
+
+export function fetchIssues({
+  owner,
+  repo,
+  page,
+  perPage = 40,
+}: {
+  owner: string;
+  repo: string;
+  page: number;
+  perPage?: number;
+}) {
+  return octokit.rest.issues.listForRepo({
+    owner,
+    repo,
+    page,
+    per_page: perPage,
+  });
+}
